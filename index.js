5e3ce1f59cc7f2676e927e3931a6b450afa73b1f
Lux <anguyen780@gmail.com>
2022-09-21 17:46:10 -0500

    created questions, questions are functional, created function to create read me

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const createMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please input your project title:',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Please input a desccription:',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Please input installation instructions:',
+            name: 'instructions'
+        },
+        {
+            type: 'input',
+            message: 'Please input any usage information:',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'List any contributors:',
+            name: 'contributors'
+        },
+        {
+            type: 'input',
+            message: 'Please input any test instructions:',
+            name: 'testInstructions'
+        },
+        {
+            type: 'list',
+            message: 'Choose a license:',
+            name: 'license',
+            choices: ['MIT','ISC','zLib']
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username:',
+            name: 'githubName'
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address:',
+            name: 'email'
+        }
+    ])
+    .then(({ title, description, instructions, usage, contributors, testInstructions, license, githubName, email}) =>
+        fs.writeFile('README.md', createMarkdown(title, description, instructions, usage, contributors, testInstructions, license, githubName, email), (err) =>
+            err ? console.error(err) : console.log('Done!'))
+    );
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,96 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = ''
+  if(license != 'None') {
+    badge = `![License Badge](https://shields.io/badge/license-${license}-green)`;
+  }
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+      return "https://www.mit.edu/~amini/LICENSE.md"
+      break;
+    case 'ISC':
+      return "https://www.isc.org/licenses/"
+      break;
+    case 'zLib':
+      return "https://www.zlib.net/zlib_license.html"
+      default: ''
+  }
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license != 'none'){
+    return `For more information please visit ${renderLicenseLink(license)}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(title, description, instructions, usage, contributors, testInstructions, license, githubName, email) {
+  return `# ${title}
+  
+## Description
+
+${description}
+
+## Table of Contents
+
+-[Installation](#installation)
+
+-[Usage](#usage)
+
+-[Contributors](#contributors)
+
+-[Testing](#testing)
+
+-[License](#license)
+
+-[Contact Me](#contact-me)
+
+## Installation
+
+${instructions}
+
+## Usage
+
+${usage}
+
+## Contributors
+
+${contributors}
+
+## Testing
+
+${testInstructions}
+
+## License
+
+${renderLicenseBadge(license)}
+
+${renderLicenseSection(license)}
+
+## Contact Me
+
+[Github](https://github.com/${githubName})
+
+Email: ${email}
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+// [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+// [![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)
